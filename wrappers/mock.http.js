664269ce9491133e8466288dbba67f9bd6216346
Daniel Beardsley <daniel@ifixit.com>
2013-09-21 14:47:23 -0700

    Add some comments

--- wrappers/mock.http.js
+++ wrappers/mock.http.js
@@ -1,11 +1,21 @@
+/**
+ * Adds a function called 'mock' to Dozuki.http if run in the browser and 
+ * and to module.exports if run in node.
+ *
+ * Used as a mock http library for passing to Dozuki during testing.
+ */
 (function(http) {
    http.mock = function(responseData) {
       var self = {};
       self.send = function(url, options) {
+
+         // Save the info that was sent for accessiblity when testing
          self.sent = {
             url: url,
             options: options
          };
+
+         // Return a mock-promise
          return {
             then: function(callback) {
                callback(responseData);
